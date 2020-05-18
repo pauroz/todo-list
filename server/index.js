f2227a60c31163fd82252ae473e04c70c0b85a14
pauroz <pau.roz@onet.pl>
2020-05-18 22:50:08 +0200

    Dodano ikone

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const path = require('path')
 const Koa = require('koa')
 const serve = require('koa-static')
-const logger = require('koa-logger')
+const logger = require('koa-logger') //l1 zad2
+const favicon = require('koa-favicon') // l1 zad3
+//const favicon = require('koa-icon') //l1 zad3
 
 const app = new Koa()
 const port = process.env.PORT || 3000
 
-app.use(logger())
+app.use(favicon(__dirname + '/favicon.ico'))//l1 zad 3
+//app.use(favicon('/resized/check.ico'))//l1 zad3
+
+app.use(logger()) //l1 zad2
 
 app.use(serve(path.resolve(__dirname, '..', 'client')))
 
